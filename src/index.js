03711b49f825519009a19298b4d1e61dd678da79
AlekseyMisuna <alexei.misuna@yandex.ru>
2025-07-24 15:50:20 +0500

    instal eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import _ from "lodash";
+import _ from 'lodash'
 export const newFunc = () => {
-    console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+  console.log('Hello, Hexlet!')
+  console.log(_.last(['one', 'two']))
 }
-
